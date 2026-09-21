959ddcd91a0a30eeff6c53dff058d6af63cd19ab
manyatasahu19-lab <manyatasahu19@gmail.com>
2026-09-21 21:29:06 +0530

    Add files via upload

---
+++ print statement.py
@@ -0,0 +1,14 @@
+#printing a greeting message
+print("Hello,world!")
+
+#printing my name
+print("My name is Manyata.")
+
+#printing a fun fact about me
+#printing("I love playing badminton")
+
+print("I love playing badminton.")
+
+#performing and printing a simple calculation
+
+print("the result of 3+5 is:", 3+5)
